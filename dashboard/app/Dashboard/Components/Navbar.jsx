e0f22659c9e970498c770decca9c7b4ee5d11569
Pasha-Ibrahim <pasaibrahimov27@gmail.com>
2026-03-13 19:32:56 +0400

    faCommentDots added

--- dashboard/app/Dashboard/Components/Navbar.jsx
+++ dashboard/app/Dashboard/Components/Navbar.jsx
@@ -3,13 +3,16 @@ import { useState } from "react";
 import user from "@/public/user.jpg";
 import Link from "next/link";
 import Image from "next/image";
-
-import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faCommentDots,
+  faSearch,
+} from "@fortawesome/free-solid-svg-icons";
+import { faBell, faComment } from "@fortawesome/free-regular-svg-icons";
 
-export default function Navbar(setIsOpen) {
-  const [isUserMenuOpen, setUserMenu] = useState(false);
+export default function Navbar({ setIsOpen }) {
+  const [isUserMenuOpen, setUserMenuOpen] = useState(false);
   const [isNotificationOpen, setNotificationOpen] = useState(false);
 
   const toggleUserMenu = () => {
@@ -28,19 +31,128 @@ export default function Navbar(setIsOpen) {
     gap-3 px-[2%]"
       >
         <div
-          className="search-box borde border-[#dfe0e4] relative h-11.25 hidden lg:flex
+          className="search-box border border-[#dfe0e4] relative h-11.25 hidden lg:flex
       items-center rounded-full w-70 outline-none"
         >
           <input
             type="text"
             placeholder="Search"
-            className="h-full w-full ps-4 outline-none"
+            className="h-full w-full ps-4
+        outline-none"
           />
           <FontAwesomeIcon
             icon={faSearch}
             className="absolute bg-[#006dca]
-      text-white right-0.5 p-3 rounded-[50%]"
+          text-white right-0.5 p-3 rounded-[50%]"
           />
+        </div>
+
+        <div
+          className="toggle lg:hidden flex cursor-pointer text-2xl"
+          onClick={() => setIsOpen(true)}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+
+        <div className="flex gap-3 items-center">
+          <span
+            className="border border-[#006dca] hover:bg-[#006dca]
+          hover:text-white px-4 rounded-full py-2 cursor-pointer
+          transition-colors duration-300 hidden lg:flex"
+          >
+            Create a New Course
+          </span>
+          <div
+            className="notification cursor-pointer border border-[#c1c4cc]
+          rounded-full min-w-11.25 hidden lg:flex justify-center
+          items-center text-xl p-2 relative hover:bg-[#006dca] hover:text-white
+          transition-colors duration-300"
+            onClick={toggleNotification}
+          >
+            <FontAwesomeIcon icon={faBell} />
+            <span
+              className="badge text-xs text-white bg-[#006dca] px-1
+            rounded-2xl absolute top-0 right-0"
+            >
+              2
+            </span>
+
+            {isNotificationOpen && (
+              <ul
+                className="absolute top-14 right-0 bg-white w-87.5 p-3 
+              flex flex-col gap-2 rounded-2xl shadow-xl animate-fade-in"
+              >
+                <li className="text-sm text-gray-700 ">
+                  <Link
+                    href="/"
+                    className="w-full flex justify-between
+                items-start"
+                  >
+                    <div className="flex items-start gap-2">
+                      <image
+                        src={user}
+                        alt="user image"
+                        className="w-14 h-14"
+                      />
+                      <div>
+                        <span className="text-[[#212529] sora-font text-lg">
+                          Ronald Richard
+                        </span>
+                        <p className="text-[#4f586d] text-xs">
+                          You can stitch between artboards
+                        </p>
+                      </div>
+                    </div>
+                    <span
+                      className="text-[#4f586d] text-xs
+                    font-medium"
+                    >
+                      23 Mins ago
+                    </span>
+                  </Link>
+                </li>
+
+                <li className="text-sm text-gray-700 ">
+                  <Link
+                    href="/"
+                    className="w-full flex justify-between
+                items-start"
+                  >
+                    <div className="flex items-start gap-2">
+                      <image
+                        src={user}
+                        alt="user image"
+                        className="w-14 h-14"
+                      />
+                      <div>
+                        <span className="text-[[#212529] sora-font text-lg">
+                          Ronald Richard
+                        </span>
+                        <p className="text-[#4f586d] text-xs">
+                          You can stitch between artboards
+                        </p>
+                      </div>
+                    </div>
+                    <span
+                      className="text-[#4f586d] text-xs
+                    font-medium"
+                    >
+                      23 Mins ago
+                    </span>
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </div>
+
+          <div
+            className="comment cursor-pointer border border-[#c1c4cc]
+          rounded-[50%] min-w-11.25 min-h-11.25 hidden lg:flex
+          justify-center text-xl p-2 relative hover:bg-[#006dca]
+          hover:text-white transition-colors duration-300"
+          >
+            <FontAwesomeIcon icon={faCommentDots} />
+          </div>
         </div>
       </div>
     </>
--- dashboard/app/Dashboard/page.jsx
+++ dashboard/app/Dashboard/page.jsx
@@ -4,7 +4,6 @@ import Sidebar from "./Components/Sidebar";
 import Navbar from "./Components/Navbar";
 import Main from "./Components/Main";
 import { isAssetError } from "next/dist/client/route-loader";
-// import { Main } from "next/document";
 
 export default function DashboardPage() {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,6 +11,13 @@ export default function DashboardPage() {
     <>
       <div className="flex min-h-screen h-100vh overflow-y-hidden">
         <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+
+        <div className="flex-1 bg-gray-100 h-screen min-h[100vh] overflow-y-scroll">
+          <Navbar setIsOpen={setIsOpen} />
+          <div className="main-content p-5 bg-[#f3f8fe]">
+            <Main />
+          </div>
+        </div>
       </div>
 
       {isOpen && (
@@ -20,13 +26,6 @@ export default function DashboardPage() {
           onClick={() => setIsOpen(false)}
         ></div>
       )}
-
-      <div className="flex-1 bg-gray-100 h-screen min-h[100vh] overflow-y-scroll">
-        <Navbar setIsOpen={setIsOpen} />
-        <div className="main-content p-5 bg-[#f3f8fe]">
-          <Main />
-        </div>
-      </div>
     </>
   );
 }
